71e36864639ceaa7aa3ffd151dca1a958d04122e
ozen-hub <info.ozenhub@gmail.com>
2023-04-01 13:07:11 +0530

    1

---
+++ src/main/java/com/icm/studentapp/repo/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.icm.studentapp.repo;
+
+import com.icm.studentapp.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@EnableJpaRepositories
+public interface StudentRepo extends JpaRepository<Student, String> {
+}
--- src/main/java/com/icm/studentapp/service/impl/StudentServiceImpl.java
+++ src/main/java/com/icm/studentapp/service/impl/StudentServiceImpl.java
@@ -3,14 +3,31 @@ package com.icm.studentapp.service.impl;
 import com.icm.studentapp.dto.paginated.PaginatedStudentResponseDto;
 import com.icm.studentapp.dto.request.RequestStudentDto;
 import com.icm.studentapp.dto.response.ResponseStudentDto;
+import com.icm.studentapp.entity.Student;
+import com.icm.studentapp.repo.StudentRepo;
 import com.icm.studentapp.service.StudentService;
+import com.icm.studentapp.util.IdManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+    private final StudentRepo studentRepo;
+    private final IdManager idManager;
+
+    public StudentServiceImpl(StudentRepo studentRepo, IdManager idManager) {
+        this.studentRepo = studentRepo;
+        this.idManager = idManager;
+    }
+
     @Override
     public void saveStudent(RequestStudentDto dto) {
-
+        Student s= new Student(
+                idManager.generate(8,"SM-S"),
+                dto.getName(),
+                dto.getAddress(),
+                dto.getSalary()
+        );
+        studentRepo.save(s);
     }
 
     @Override
